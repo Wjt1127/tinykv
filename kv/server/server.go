b02195d60cd08d9d3249a92c6c0cae5a28938e8e
Wjt1127 <1243254658@qq.com>
2024-02-17 16:21:34 +0800

    (progress) : pass project4b

--- kv/server/server.go
+++ kv/server/server.go
@@ -7,6 +7,7 @@ import (
 	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/kv/storage/raft_storage"
 	"github.com/pingcap-incubator/tinykv/kv/transaction/latches"
+	"github.com/pingcap-incubator/tinykv/kv/transaction/mvcc"
 	coppb "github.com/pingcap-incubator/tinykv/proto/pkg/coprocessor"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/tinykvpb"
@@ -48,19 +49,241 @@ func (server *Server) Snapshot(stream tinykvpb.TinyKv_SnapshotServer) error {
 }
 
 // Transactional API.
+// 获取单个 key 的 Value。
+// 1. 获取 Lock，如果 Lock 的 startTs 小于当前的 startTs，说明存在之前存在尚未 commit 的请求，中断操作，返回 LockInfo；
+// 2. 否则直接获取 Value，如果 Value 不存在，则设置 NotFound = true；
 func (server *Server) KvGet(_ context.Context, req *kvrpcpb.GetRequest) (*kvrpcpb.GetResponse, error) {
 	// Your Code Here (4B).
-	return nil, nil
+	resp := &kvrpcpb.GetResponse{}
+
+	// 获取 reader
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			resp.RegionError = regionErr.RequestErr
+			return resp, nil
+		}
+		return resp, err
+	}
+
+	// 创建 txn，获取 Lock
+	txn := mvcc.NewMvccTxn(reader, req.Version) // 创建读事务txn，其中的 TS 使用 req.Version
+	lock, err := txn.GetLock(req.Key)
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			resp.RegionError = regionErr.RequestErr
+			return resp, nil
+		}
+		return resp, err
+	}
+
+	// Percolator 为了保证快照隔离的时候总是能读到已经 commit 的数据
+	// 当发现准备读取的数据被锁定的时候，会等待解锁
+	if lock != nil && req.Version >= lock.Ts {
+		resp.Error = &kvrpcpb.KeyError{
+			Locked: &kvrpcpb.LockInfo{
+				PrimaryLock: lock.Primary,
+				LockVersion: lock.Ts,
+				Key:         req.Key,
+				LockTtl:     lock.Ttl,
+			},
+		}
+	}
+
+	// 还需要判断这个 key 是否存在 storage 中
+	value, err := txn.GetValue(req.Key)
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			resp.RegionError = regionErr.RequestErr
+			return resp, nil
+		}
+		return resp, err
+	}
+	if value == nil {
+		resp.NotFound = true
+	}
+	resp.Value = value
+
+	return resp, nil
 }
 
+// Percolator 第一阶段 preWrite
+// 1. 通过 MostRecentWrite 检查所有 key 的最新 Write，如果存在，且其 commitTs 大于当前事务的 startTs，说明存在 write conflict，终止操作；
+// 2. 通过 GetLock() 检查所有 key 是否有 Lock，如果存在 Lock，说明当前 key 被其他事务使用中，终止操作；
+// 3. 到这一步说明可以正常执行 Prewrite 操作了，写入 Default 数据和 Lock；
 func (server *Server) KvPrewrite(_ context.Context, req *kvrpcpb.PrewriteRequest) (*kvrpcpb.PrewriteResponse, error) {
 	// Your Code Here (4B).
-	return nil, nil
+	resp := &kvrpcpb.PrewriteResponse{}
+
+	// 获取 reader
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			resp.RegionError = regionErr.RequestErr
+			return resp, nil
+		}
+		return resp, err
+	}
+	defer reader.Close()
+	// 创建 txn
+	txn := mvcc.NewMvccTxn(reader, req.StartVersion) // 创建读事务txn，其中的 TS 使用 req.StartVersion
+
+	// 1. 检查在 startTS 到现在这段时间内，key 中最新的 Write 是否有 commitTS 大于当前事务的 startTS
+	//    利用 4A 中的 MostRecentWrite 获取 key 的最新 Write
+	var keyErrors []*kvrpcpb.KeyError
+	for _, operation := range req.Mutations {
+		write, commitTS, err := txn.MostRecentWrite(operation.Key)
+		if err != nil {
+			if regionErr, ok := err.(*raft_storage.RegionError); ok {
+				resp.RegionError = regionErr.RequestErr
+				return resp, nil
+			}
+			return resp, err
+		}
+		// 检测在 StartTS 之后是否有已经提交的 Write，如果有的话说明写冲突，需要 abort 当前的事务
+		if write != nil && commitTS >= req.StartVersion {
+			keyErrors = append(keyErrors, &kvrpcpb.KeyError{
+				Conflict: &kvrpcpb.WriteConflict{
+					StartTs:    req.StartVersion,
+					ConflictTs: commitTS,
+					Key:        operation.Key,
+					Primary:    req.PrimaryLock,
+				},
+			})
+			continue
+		}
+
+		// 检测 Key 是否有 Lock 锁住，如果有的话则说明别的事务可能正在修改并且未提交
+		lock, err := txn.GetLock(operation.Key)
+		if err != nil {
+			if regionErr, ok := err.(*raft_storage.RegionError); ok {
+				resp.RegionError = regionErr.RequestErr
+				return resp, nil
+			}
+			return resp, err
+		}
+		if lock != nil {
+			keyErrors = append(keyErrors, &kvrpcpb.KeyError{
+				Locked: &kvrpcpb.LockInfo{
+					PrimaryLock: req.PrimaryLock,
+					LockVersion: lock.Ts,
+					Key:         operation.Key,
+					LockTtl:     lock.Ttl,
+				},
+			})
+			continue
+		}
+
+		// 暂存修改到 txn 中，然后对需要修改的 Key 进行加锁
+		var kind mvcc.WriteKind
+		switch operation.Op {
+		case kvrpcpb.Op_Put:
+			kind = mvcc.WriteKindPut
+			txn.PutValue(operation.Key, operation.Value)
+		case kvrpcpb.Op_Del:
+			kind = mvcc.WriteKindDelete
+			txn.DeleteValue(operation.Key)
+		case kvrpcpb.Op_Rollback: // 直接返回了
+			return nil, nil
+		}
+
+		txn.PutLock(operation.Key, &mvcc.Lock{
+			Primary: req.PrimaryLock,
+			Ts:      req.StartVersion,
+			Ttl:     req.LockTtl,
+			Kind:    kind,
+		})
+	}
+
+	// 说明有冲突
+	if len(keyErrors) > 0 {
+		resp.Errors = keyErrors
+		return resp, nil
+	}
+
+	// 写入事务中暂存的修改到 storage 中
+	err = server.storage.Write(req.Context, txn.Writes())
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			resp.RegionError = regionErr.RequestErr
+			return resp, nil
+		}
+		return nil, err
+	}
+
+	return resp, nil
 }
 
+// 1. 通过 Latches 上锁对应的 key；
+// 2. 尝试获取每一个 key 的 Lock，并检查 Lock.StartTs 和当前事务的 startTs 是否一致，不一致直接取消。因为存在这种情况，客户端 Prewrite 阶段耗时过长，Lock 的 TTL 已经超时，被其他事务回滚，所以当客户端要 commit 的时候，需要先检查一遍 Lock；
+// 3. 如果成功则写入 Write 并移除 Lock；
 func (server *Server) KvCommit(_ context.Context, req *kvrpcpb.CommitRequest) (*kvrpcpb.CommitResponse, error) {
 	// Your Code Here (4B).
-	return nil, nil
+	resp := &kvrpcpb.CommitResponse{}
+
+	// 获取 reader
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			resp.RegionError = regionErr.RequestErr
+			return resp, nil
+		}
+		return resp, err
+	}
+	defer reader.Close()
+	// 创建 txn
+	txn := mvcc.NewMvccTxn(reader, req.StartVersion) // 创建读事务txn，其中的 TS 使用 req.StartVersion
+
+	// 1. 通过 latch 对对应的 key 上锁
+	server.Latches.WaitForLatches(req.Keys)
+	defer server.Latches.ReleaseLatches(req.Keys)
+
+	for _, key := range req.Keys {
+		// 2. 检测是否是重复提交（测试程序需要）
+		write, _, err := txn.CurrentWrite(key) // 获取最新的 write
+		if err != nil {
+			if regionErr, ok := err.(*raft_storage.RegionError); ok {
+				resp.RegionError = regionErr.RequestErr
+				return resp, nil
+			}
+			return nil, err
+		}
+		// Rollback 类型的 Write 表示是已经正确提交的，这个时候按照重复提交处理
+		if write != nil && write.Kind != mvcc.WriteKindRollback && write.StartTS == req.StartVersion {
+			return resp, nil
+		}
+		// 3. 检查每个 Key 的 Lock 是否还存在
+		lock, err := txn.GetLock(key)
+		if err != nil {
+			if regionErr, ok := err.(*raft_storage.RegionError); ok {
+				resp.RegionError = regionErr.RequestErr
+				return resp, nil
+			}
+			return nil, err
+		}
+		// 如果 Lock 不存在，有两种情况：
+		// 第一种是事务已经正确提交了，这次是一个重复提交；
+		// 第二种是这个事务前面的 prewrite 操作因为过于缓慢，超时，导致你的 lock 被其他事务 rollback 了
+		if lock == nil || lock.Ts != req.StartVersion {
+			resp.Error = &kvrpcpb.KeyError{Retryable: "true"}
+			return resp, nil
+		}
+		// 3. 第一次提交事务，正常处理
+		txn.PutWrite(key, req.CommitVersion, &mvcc.Write{
+			StartTS: req.StartVersion,
+			Kind:    lock.Kind,
+		})
+		txn.DeleteLock(key)
+	}
+	err = server.storage.Write(req.Context, txn.Writes())
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			resp.RegionError = regionErr.RequestErr
+			return resp, nil
+		}
+		return nil, err
+	}
+	return resp, nil
 }
 
 func (server *Server) KvScan(_ context.Context, req *kvrpcpb.ScanRequest) (*kvrpcpb.ScanResponse, error) {
